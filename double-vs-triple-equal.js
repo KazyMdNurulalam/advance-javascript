fc8110ab7a2a8e78c4719bfb03b4608ad21eefdf
KazyMdNurulalam <kazymdnurulalam@gmail.com>
2021-02-06 05:14:40 +0600

    double and triple equal confution clear

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,10 @@
+// double equal check only value.
+// triple equal check value and types.
+const x = 2;
+const y = "2";
+if(x == y){
+    console.log("condition in true");
+}
+else{
+    console.log("condition is false");
+}
